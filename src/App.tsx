97eeaf31b3f678ea249611540e279de2df5356b5
youssefamerzag <amerzag.youssef4@gmail.com>
2024-07-30 13:02:13 +0100

    fix design and add buy me coffee

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
+import KofiButton from 'kofi-button';
 
 interface Coin {
   id: string;
@@ -21,8 +22,8 @@ function App() {
     <div className='container'>
       <div className='card'>
         <div className='links'>
-          <a href='https://github.com/youssefamerzag'><img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/github.png" alt="github"/></a>
-          <a href='https://www.linkedin.com/in/youssefamerzag/'><img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/linkedin.png" alt="linkedin"/></a>
+          <a href='https://github.com/youssefamerzag' target="_blank"><img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/github.png" alt="github" /></a>
+          <a href='https://www.linkedin.com/in/youssefamerzag/' target="_blank"><img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/linkedin.png" alt="linkedin" /></a>
           <p>Crypto Calculator Assistant</p>
         </div>
         <form className='form'>
@@ -33,20 +34,23 @@ function App() {
             <select className='select' onChange={(e) => setUsd(Number(e.target.value))} value={usd}>
               <option value='1'>USD</option>
             </select>
-            <input className='input' type='number' onChange={(e) => setUsd(Number(e.target.value))} ></input>
+            <input className='input' type='number' onChange={(e) => setUsd(Number(e.target.value))}></input>
           </div>
           <div>
             <p className='label'>You get</p>
           </div>
           <div className='input-group'>
             <select className='select' onClick={(e) => setCoinValue(Number((e.target as HTMLSelectElement).value))}>
-              {coins.map((coin, index) => 
+              {coins.map((coin, index) =>
                 <option key={index} value={coin.priceUsd}>{coin.id}</option>
               )}
             </select>
             <input className='input' placeholder='Choose' type='number' value={coinValue !== 0 ? (usd / coinValue) : ''} readOnly></input>
           </div>
         </form>
+        <div>
+          <KofiButton color="#0a9396" title="Support Me on Ko-fi" kofiID="D1D811635P" />
+        </div>
       </div>
     </div>
   );
